cdc4d693280e62f03462db06a5ecca845365caa3
rainskin <homemadepunk@yandex.ru>
2024-04-24 13:18:07 +0500

    emergency fixed bug with '<' symbol which is read as the beginning of an open tag in "parse mode html"

--- src/handlers/ad_schedule/get_ad_post.py
+++ src/handlers/ad_schedule/get_ad_post.py
@@ -8,12 +8,14 @@ import keyboards
 from loader import dp, userbot, bot
 from states import States
 from utils import links
+from utils.msg_marker import get_marker
 
 
 @dp.message_handler(state=States.waiting_ad_post, content_types="any")
 async def _(msg: types.Message, state: FSMContext):
     msg_text = msg.caption or msg.text
     title = msg_text[0:40] if msg_text else 'Вложение'
+    title = title.replace("<", " ")
 
     data = await state.get_data()
     service_msg_id = data.get('service_msg_id')
@@ -23,6 +25,7 @@ async def _(msg: types.Message, state: FSMContext):
     if msg.media_group_id:
         group_id_in_data = data.get('group_id')
         message_ids = data.get('message_id', [])
+        marker = get_marker(msg)
         if group_id_in_data == msg.media_group_id:
             return
 
@@ -38,9 +41,10 @@ async def _(msg: types.Message, state: FSMContext):
                                         parse_mode='html', disable_web_page_preview=True)
 
     else:
-
+        marker = get_marker(msg)
         await state.update_data(message_id=msg.message_id, is_album=False)
         ad_title_text = f'Пост <b>"{title}..."</b> принят'
+        print(title)
         await asyncio.sleep(2)
         ad_title_msg = await msg.answer(ad_title_text, parse_mode='html',
                                         reply_markup=keyboards.AdPostSettings(drop_author=drop_author,
@@ -48,9 +52,11 @@ async def _(msg: types.Message, state: FSMContext):
                                         disable_web_page_preview=True)
 
     link = await links.get_links_from_msg(msg)
+    print(f'Маркер = {marker}')
 
-    await state.update_data(drop_author=drop_author, notification=notification, link=link, title=title, ad_title_msg_id=ad_title_msg.message_id,
-                            ad_title_text=ad_title_text)
+    await state.update_data(drop_author=drop_author, notification=notification, link=link, title=title,
+                            ad_title_msg_id=ad_title_msg.message_id,
+                            ad_title_text=ad_title_text, marker=marker)
     await bot.delete_message(msg.chat.id, service_msg_id)
     await States.schedule_ad.set()
 
