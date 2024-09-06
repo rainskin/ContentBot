54382a3542432f8a13e70ae46948f6e770913636
rainskin <homemadepunk@yandex.ru>
2024-09-06 10:56:28 +0500

    reverse commit

--- src/__main__.py
+++ src/__main__.py
@@ -9,15 +9,9 @@ handlers.setup()
 
 
 async def on_startup(dp):
-    print("on_startup: Starting background tasks...")  # Лог перед стартом задач
     await background_tasks.start()
-    print("on_startup: Background tasks started.")  # Лог после старта задач
-
-    print("on_startup: Setting up ad manager...")  # Лог перед настройкой ad_manager
     await ad_manager.setup()
-    print("on_startup: Ad manager setup complete.")  # Лог после настройки ad_manager
-
-    print('on_startup: Задачи запущены')
+    print('Задачи запущены')
 
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
