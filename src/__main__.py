2e223bf67091df276884874727969083424a12b1
rainskin <homemadepunk@yandex.ru>
2024-09-06 10:55:48 +0500

    try catch bug

--- src/__main__.py
+++ src/__main__.py
@@ -9,9 +9,15 @@ handlers.setup()
 
 
 async def on_startup(dp):
+    print("on_startup: Starting background tasks...")  # Лог перед стартом задач
     await background_tasks.start()
+    print("on_startup: Background tasks started.")  # Лог после старта задач
+
+    print("on_startup: Setting up ad manager...")  # Лог перед настройкой ad_manager
     await ad_manager.setup()
-    print('Задачи запущены')
+    print("on_startup: Ad manager setup complete.")  # Лог после настройки ad_manager
+
+    print('on_startup: Задачи запущены')
 
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
